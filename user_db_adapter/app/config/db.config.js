8f8ffa45e5a3196ed264e6fa1d0e974fa1e2ec6a
vicentinileonardo <vicentini.leonardo99@gmail.com>
2023-02-20 11:57:43 +0100

    user_db_adapter first commit

---
+++ user_db_adapter/app/config/db.config.js
@@ -0,0 +1,11 @@
+const {
+    USER_DB_USER,
+    USER_DB_PASSWORD,
+    USER_DB_HOST,
+    USER_DB_DOCKER_PORT,
+    USER_DB_NAME,
+  } = process.env;
+  
+  module.exports = {
+    url: `mongodb://${USER_DB_USER}:${USER_DB_PASSWORD}@${USER_DB_HOST}:${USER_DB_DOCKER_PORT}/${USER_DB_NAME}?authSource=admin`
+  };
