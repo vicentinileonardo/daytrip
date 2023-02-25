4b86aface07eb9a3641dc780b1cd952d4ed3ada3
vicentinileonardo <vicentini.leonardo99@gmail.com>
2023-02-25 14:54:36 +0100

    change in test.sh

--- test.sh
+++ test.sh
@@ -13,16 +13,18 @@ docker build -t $SERVICE_NAME $DOCKERFILE_PATH
 
 # Start a new container based on the same image as the service container
 docker run --name $TEST_CONTAINER_NAME \
-            #--network=host \
+            --network=host \
+            -p 16870:8080 \
             --env-file ./.env \
             -e NODE_ENV=test \
+            -e PROVA=prova \
             -d $SERVICE_NAME 
             
 # Copy your test files into the container
 docker cp $TESTS_FOLDER_PATH $TEST_CONTAINER_NAME:/$SERVICE_NAME/
 
 #stop node server due to port conflict
-#docker exec $TEST_CONTAINER_NAME sh -c 'pkill -f "node /forecast_api_adapter/server.js"'
+docker exec $TEST_CONTAINER_NAME sh -c 'pkill -f "node /forecast_api_adapter/server.js"'
 
 # Run your tests inside the container
 #docker exec $TEST_CONTAINER_NAME npm test
