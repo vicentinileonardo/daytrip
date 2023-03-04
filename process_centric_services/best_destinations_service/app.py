9da66e64fce13985fc5c6647d68a2c9141f4bc5a
vicentinileonardo <vicentini.leonardo99@gmail.com>
2023-03-05 00:50:29 +0100

    updated homepage

--- process_centric_services/best_destinations_service/app.py
+++ process_centric_services/best_destinations_service/app.py
@@ -7,6 +7,16 @@ import re
 
 app = Flask(__name__)
 
+'''
+# allow CORS
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+'''
+
 @app.route("/", methods=["GET"])
 def check():
     
